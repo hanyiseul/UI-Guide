7aa8257ce6c292b7a998070de0fd5999897826ad
hanyiseul <seulli_hys@daum.net>
2023-09-16 20:13:12 +0900

    header 수정

--- assets/js/uid.js
+++ assets/js/uid.js
@@ -1,34 +1,23 @@
-//includeHTML
 function includeHTML(){
-  let z, elmnt, file, xhttp;
-
-  z = document.getElementsByTagName("*");
-  
-  for (let i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    file = elmnt.getAttribute("data-include");
-    
-    if (file) {
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("data-include");
-          includeHTML();
-        }//if
-      }//onreadystatechange
-
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      return;
-    }//if - file
-  }//for
-}
-
-
-/* 실행 */
+    let allElements = document.getElementsByTagName('*');
+      Array.prototype.forEach.call(allElements, function(el) {
+        let includePath = el.dataset.includePath;
+        if (includePath) {
+          let xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+              el.outerHTML = this.responseText;
+            }
+          };
+          xhttp.open('GET', includePath, true);
+          xhttp.send();
+        }
+      });
+}//includeHTML
+ 
+ 
+/* ✨ 실행 */
 window.addEventListener('DOMContentLoaded',()=>{
-  includeHTML();
+    includeHTML();
 });
+ 
