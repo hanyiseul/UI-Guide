96a42efc9e3afaef896edd77979603c9ac5cbc47
seulli <seulli_hys@daum.net>
2023-11-12 10:14:43 +0900

    경로수정

--- assets/js/uid.js
+++ assets/js/uid.js
@@ -1,41 +1,22 @@
 (() => {
   // layout 호출
-  function includeHTML(callback) {
-    var z, i, elmnt, file, xhr;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("include-html");
-      //console.log(file);
-      if (file) {
-        /*make an HTTP request using the attribute value as the file name:*/
-        xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {
-              elmnt.innerHTML = this.responseText;
-            }
-            if (this.status == 404) {
-              elmnt.innerHTML = "Page not found.";
-            }
-            /*remove the attribute, and call this function once more:*/
-            elmnt.removeAttribute("include-html");
-            includeHTML(callback);
-          }
+  function includeHTML(){
+    let allElements = document.getElementsByTagName('*');
+      Array.prototype.forEach.call(allElements, function (el) {
+        let includePath = el.dataset.includePath;
+        if (includePath) {
+          let xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+              el.outerHTML = this.responseText;
+            };
+          };
+          xhttp.open('GET', includePath, true);
+          xhttp.send();
         };
-        xhr.open("GET", file, true);
-        xhr.send();
-        /*exit the function:*/
-        return;
-      }
+      });
     }
-    setTimeout(function() {
-      callback();
-    }, 0);
-  }
-  includeHTML();
+    includeHTML();
 
     // dropdown
     function dropdown() {
