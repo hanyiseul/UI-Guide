3ff9f89916cde2013e3fbac2d1f63b0fc18859e8
seulli <seulli_hys@daum.net>
2023-11-24 23:12:32 +0900

    progressbar 컴포넌트 작업

--- assets/js/uid.js
+++ assets/js/uid.js
@@ -126,4 +126,28 @@
       })
     }
     modal();
+
+    // progress bar scroll
+    function progress () {
+      const progress = document.querySelector('.progress.is-scroll .progress__bar');
+      console.log(progress);
+
+      const scrollEvent = () => {
+        const scrollTop = document.documentElement.scrollTop;
+        const scrollHeight = document.documentElement.scrollHeight;
+        const clientHeight = document.documentElement.clientHeight;
+
+        const progressBar = (scrollTop / (scrollHeight - clientHeight))  * 100
+
+        console.log('scrollTop:',scrollTop)
+        console.log('scrollHeight:',scrollHeight)
+        console.log('clientHeight:',clientHeight)
+        console.log('progressbar:',progressBar)
+
+        progress.style.width = `${progressBar}%`
+        console.log(progressBar);
+      }
+      window.addEventListener('scroll', () => scrollEvent());
+    }
+    progress();
 })();
