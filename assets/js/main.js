d0df152ea00658b78e684138b4b3e66a2a930d36
hanyiseul <seulli_hys@daum.net>
2024-06-10 11:39:33 +0900

    프로필 정리

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,35 @@
 (() => { 
+  if(window.innerWidth > 1024) {
+  //   const content = document.querySelector('.main__content');
+  //   const section = document.querySelectorAll('.main__content section')
+  //   let page = 0;
+  //   let lastPage = section.length - 1;
+  //   let wheeling;
 
+  // content.addEventListener("wheel", (e) => {
+  //   console.log('hi');
+  //   if(e.deltaY > 0) {
+  //     console.log('hi')
+  //     page++;
+  //     if(page > lastPage) {
+  //       page = lastPage;
+  //     }
+  //     console.log('mousedown', page)
+  //     content.style.top = page * -100 + 'vh'
+  //   }
 
-    // const elm = document.querySelectorAll("section");
-    // const elmCount = elm.length;
-    // elm.forEach((item, index) => {
-    //   item.addEventListener("wheel", ((e) => {
-    //     e.preventDefault();
-    //     let delta = 0;
-    //     if (!e) e = window.e;
-    //     if (e.wheelDelta) {
-    //       delta = e.wheelDelta / 120;
-    //       if (window.opera) delta = -delta;
-    //     } else if (e.detail) delta = -e.detail / 3;
-
-    //     let moveTop = window.scrollY;
-    //     let elmSelector = elm[index];
-
-    //     // wheel down : move to next section
-    //     if (delta < 0) {
-    //       if (elmSelector !== elmCount - 1) {
-    //         try {
-    //           moveTop =
-    //             window.pageYOffset +
-    //             elmSelector.nextElementSibling.getBoundingClientRect().top;
-    //         } catch (e) {}
-    //       }
-    //     }
-
-    //     // wheel up : move to previous section
-    //     else {
-    //       if (elmSelector !== 0) {
-    //         try {
-    //           moveTop =
-    //             window.pageYOffset +
-    //             elmSelector.previousElementSibling.getBoundingClientRect().top;
-    //         } catch (e) {}
-    //       }
-    //     }
-    //     window.scrollTo({ top: moveTop, left: 0,});
-    //   }));
-    // });
-
-    // function debounce(func, delay) {
-    //   let timer;
-    //   return function() {
-    //       const args = arguments;
-    //       clearTimeout(timer);
-    //       timer = setTimeout(() => {
-    //           func.apply(this, args);
-    //       }, delay);
-    //   }
-    // }
-
+  //   if(e.deltaY < 0) {
+  //     console.log('hi')
+  //     page--;
+  //     if(page < 0) {
+  //       page = 0;
+  //     }
+  //     console.log('mouseup', page)
+  //     content.style.top = page * -100 + 'vh'
+  //   }
+  // }, {passive: false})
+  // }
+  
 })();
 
   
